227fa7547adba96acb69d0cb122034dca4c28642
Madhankannan02 <madhankannan29@gmail.com>
2026-01-17 08:49:10 +0530

    Build Sentence Maker

---
+++ Javascript Code/Variables and Strings/03.Build-Sentence-Maker.js
@@ -0,0 +1,27 @@
+// 1. Declare variables using 'let' and assign initial values
+let adjective = "purple";
+let noun = "dragon";
+let verb = "dancing";
+let place = "castle";
+let adjective2 = "gigantic";
+let noun2 = "tacos";
+
+// 2. Create the first story using the template
+let firstStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 3. Output the first story
+console.log("First story: " + firstStory);
+
+// 4. Reassign new values to the variables for the second story
+adjective = "tiny";
+noun = "hamster";
+verb = "sleeping";
+place = "shoe box";
+adjective2 = "microscopic";
+noun2 = "blueberries";
+
+// 5. Create the second story using the same template
+let secondStory = "Once upon a time, there was a(n) " + adjective + " " + noun + " who loved to eat " + noun2 + ". The " + noun + " lived in a " + place + " and had " + adjective2 + " nostrils that blew fire when it was " + verb + ".";
+
+// 6. Output the second story
+console.log("Second story: " + secondStory);
