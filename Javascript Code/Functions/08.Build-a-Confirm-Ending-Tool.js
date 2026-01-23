c6fbec78ef086adb69f5d46f5afab7ecac3d4864
Madhankannan02 <madhankannan29@gmail.com>
2026-01-23 18:49:54 +0530

    08.Build-a-Confirm-Ending-Tool

---
+++ Javascript Code/Functions/08.Build-a-Confirm-Ending-Tool.js
@@ -0,0 +1,17 @@
+function confirmEnding (strng, strngCheck){
+  let lastIndex = strng.length;
+  //console.log(lastIndex);
+
+  let lastIndexofStrng = strngCheck.length;
+  //console.log(lastIndexofStrng);
+
+  let lastLetter = strng.slice(lastIndex-lastIndexofStrng,lastIndex);
+
+  return (lastLetter === strngCheck);
+}
+
+confirmEnding("Bastian", "n");
+confirmEnding("Congratulation", "on")
+confirmEnding("Connor", "n")
+confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")
+confirmEnding("Abstraction", "action")
