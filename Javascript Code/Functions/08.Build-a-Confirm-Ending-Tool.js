423642c41da767ced5001f995e3b69d04184d3fa
Madhankannan02 <madhankannan29@gmail.com>
2026-01-24 18:57:47 +0530

    Completed functions

--- Javascript Code/Functions/08.Build-a-Confirm-Ending-Tool.js
+++ Javascript Code/Functions/08.Build-a-Confirm-Ending-Tool.js
@@ -14,4 +14,3 @@ confirmEnding("Bastian", "n");
 confirmEnding("Congratulation", "on")
 confirmEnding("Connor", "n")
 confirmEnding("Walking on water and developing software from a specification are easy if both are frozen", "specification")
-confirmEnding("Abstraction", "action")
